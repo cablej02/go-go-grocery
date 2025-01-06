f5b4fd7757715b97a067cc245f2af0e370ff9860
Jason Cable <cablej02@gmail.com>
2025-01-06 15:47:20 -0600

    added footer, styling, and bootstrap import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { Outlet } from 'react-router-dom';
+import './App.css';
 
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
 
@@ -10,6 +12,7 @@ function App() {
       <main className='container pt-5'>
         <Outlet />
       </main>
+      <Footer />
     </div>
   )
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+
+const Footer = () => {
+    const developers = [
+        { name: 'Mauricio', github: 'https://github.com/mauriciomdlg' },
+        { name: 'Nattan', github: 'https://github.com/nattanaguiat' },
+        { name: 'Jordan', github: 'https://github.com/JL-Code1' },
+        { name: 'Jason', github: 'https://github.com/cablej02' },
+    ];
+
+    return (
+        <footer className="footer bg-dark text-white text-center py-3">
+            <div>
+                {developers.map((dev, index) => (
+                    <span key={index} className="mx-2">
+                        <a
+                            href={dev.github}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-white text-decoration-none"
+                        >
+                            <FaGithub size={30} />
+                        </a>
+                    </span>
+                ))}
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
