76a0fee3ee7a73967c6ff693cbc48c0825f3dca9
Mauricio Martinez <mauriciomtzdlg@gmail.com>
2025-01-08 20:39:00 -0600

    added dropdown for grocery list

--- client/src/components/GroceryList.jsx
+++ client/src/components/GroceryList.jsx
@@ -1,9 +1,31 @@
+import React, { useState } from "react";
+
 const GroceryList = ({ lists }) => {
+    const [selectedList, setSelectedList] = useState(null);
+
+    const handleChange = (e) => {
+        setSelectedList(e.target.value);
+    }
+
     console.log(lists)
     return (
-        <>
-            <p> on this thing</p>
-        </>
+        <div className="grocery-list pt-5">
+            <select 
+            id="list-select"
+            value={selectedList || ""}
+            onChange={handleChange}
+            >
+                <option value="" disabled>
+                    Select a List
+                </option>
+                {lists.map((list, index) => (
+                    <option key={index} value={list.id}>
+                        {list.name}
+                    </option>
+                ))}
+            </select>
+        </div>
+        
     )
 }
 export default GroceryList;
