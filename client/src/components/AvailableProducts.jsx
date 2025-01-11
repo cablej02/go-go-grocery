d7a98c73549cce4467d46b99928fc220c469f20e
Jason Cable <cablej02@gmail.com>
2025-01-11 12:55:27 -0600

    added search field for available products and basic styling

--- client/src/components/AvailableProducts.jsx
+++ client/src/components/AvailableProducts.jsx
@@ -1,6 +1,20 @@
+import React, { useState } from 'react';
 import { createGroceryListItem } from '../api/groceryListAPI.jsx';
 
+import '../styles/availableProductsStyles.css';
+
 const AvailableProducts = ({ products, selectedList, listItems, setListItems }) => {
+    const [searchInput, setSearchInput] = useState('');
+
+    const handleSearchChange = (e) => {
+        // TODO: add delay if needed
+        setSearchInput(e.target.value.toLowerCase());
+    }
+
+    const filteredProducts = products.filter((product) => {
+        return product.name.toLowerCase().includes(searchInput);
+    });
+
     const handleAddListItem = async (listId, productId) => {
         try {
             const result = await createGroceryListItem(listId, productId, 1);
@@ -18,11 +32,21 @@ const AvailableProducts = ({ products, selectedList, listItems, setListItems }) 
     }
 
     return (
-        <div>
+        <div className="d-flex flex-column justify-content-center mx-auto">
             <h3>Available Products</h3>
-            <ul className="list-group">
-                {products.map((product) => (
-                    <li key={product.id} className="list-group-item">
+            {/* Search Input */}
+            <input
+                type="text"
+                placeholder="Search Products..."
+                value={searchInput}
+                onChange={handleSearchChange}
+                className="search-input form-control mb-3 bg-light mx-auto"
+                style={{ width: '300px' }}
+            />
+            {/* List of Available Products */}
+            <ul className="list-group mx-auto">
+                {filteredProducts .map((product) => (
+                    <li key={product.id} className="list-group-item gap-5">
                         <button onClick={() => handleAddListItem(selectedList, product.id)}>Add</button>
                         <span>{product.name}</span>
                     </li>
