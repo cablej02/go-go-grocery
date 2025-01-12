67fe6365e9406d78c4d5e6ca70ddc5784af725d7
Jason Cable <cablej02@gmail.com>
2025-01-12 13:16:06 -0600

    updated server to serve index.html for all unmatched routes

--- server/src/server.js
+++ server/src/server.js
@@ -4,15 +4,27 @@ dotenv.config();
 import express from 'express';
 import sequelize from './config/connection.js';
 import routes from './routes/index.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Define __dirname to be the directory of the current file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Serves static files in the entire client's dist folder
-app.use(express.static('../client/dist')); //TODO: update path
+const clientDistPath = path.join(__dirname, '../client/dist');
+app.use(express.static(clientDistPath)); 
 
 app.use(express.json());
 app.use(routes);
+
+// Serves the index.html file for any unknown routes
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(clientDistPath, 'index.html'));
+});
 
 sequelize.sync().then(() => {
   app.listen(PORT, () => {
